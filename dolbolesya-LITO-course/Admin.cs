86d8ea7cb37603a7ee373c6febca8036a97622bc
dolbolesya <zylynzy@gmail.com>
2022-04-21 17:58:43 +0300

    Добавьте файлы проекта.

---
+++ dolbolesya-LITO-course/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolbolesya_LITO_course
+{
+    class Admin:User
+    {
+        public Admin Vlad = new Admin()
+        {
+           
+
+        };
+    }
+}
---
+++ dolbolesya-LITO-course/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolbolesya_LITO_course
+{
+    class User
+    {
+        public uint id;
+        public string? name
+        {
+            get { return name; }
+            set { name = value; }   
+        }
+        public int age;
+
+        public string? profession;
+        private int salary { get; set; }
+
+
+    }
+}
